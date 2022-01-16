c91a4cce3ebeca1e2d10567b87aeb2c45d4d94cf
BartJanvanRo <hjavanrossum@gmail.com>
2022-01-16 22:51:37 +0100

    16 januari

--- firebase.js
+++ firebase.js
@@ -3,17 +3,25 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBx83WUVgovXHFKx-5Lxm75UdyvLnNR5_U",
-    authDomain: "to-do-live-2.firebaseapp.com",
-    projectId: "to-do-live-2",
-    storageBucket: "to-do-live-2.appspot.com",
-    messagingSenderId: "538854901792",
-    appId: "1:538854901792:web:7a64babd69032a4912d445",
-    measurementId: "G-2CTHJKTHXW"
+
+    apiKey: "AIzaSyCs99Z0jZpyyefdrHQ-xJvluOX4M1SVsYM",
+
+    authDomain: "todo-1801c.firebaseapp.com",
+
+    projectId: "todo-1801c",
+
+    storageBucket: "todo-1801c.appspot.com",
+
+    messagingSenderId: "275124322598",
+
+    appId: "1:275124322598:web:ebc7eef1f52444b91b41e7",
+
+    measurementId: "G-Y19ESMLLWE"
+
 };
 
 
-// Initialize Firebase
+// Initialize Firebase 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 var db = firebase.firestore();
--- script.js
+++ script.js
@@ -1,12 +1,94 @@
 
-function addItem(event) {
-  event.preventDefault();
-  let text = document.getElementById("todo-input");
-  db.collection("todo-items").add({
-    text: text.value,
-    status: "active"
-  });
-  text.value = "";
+
+function getItems() {
+  db.collection("todo-items").onSnapshot((snapshot) => {
+    let items = [];
+    snapshot.docs.forEach((doc) => {
+      items.push({
+        id: doc.id,
+        ...doc.data()
+      })
+    })
+    generateItems(items);
+  })
 }
 
+/* hiermee wordt de textbalk leeggemaakt.  */
+
+
 /* db = database */
+
+//items ophalen. navragen wat onSnap en doc.id enzo bedoelen. 
+function getItems() {
+  db.collection("todo-items").onSnapshot((snapshot) => {
+    let items = [];
+    snapshot.docs.forEach((doc) => {
+      items.push({
+        id: doc.id,
+        ...doc.data()
+      })
+    })
+    generateItems(items); //spreekt de volgende functie aan
+  })
+}
+
+
+
+function generateItems(items) {
+  let itemsHTML = ""
+  items.forEach((item) => {
+    /* console.log(item)  lijn 37 ? is een if/else statement kort geschreven*/
+    itemsHTML += `
+    <div class="todo-item">
+              <div class="check">                                             
+              <div data-id="${item.id} class="check-mark ${item.status} == "completed" && "checked" ? "checked": ""  >
+      <img src="assets/icon-check.svg">
+      </div>
+              </div >
+      <div class="todo-text ${item.status} == "completed" && "checked" ? "checked": "">
+        ${item.text}
+      </div>
+            </div >
+
+
+      `
+  })
+  document.querySelector(".todo-items").innerHTML = itemsHTML
+
+}
+
+
+//vervangt de todo-items met info vanuit database
+
+function createEventListeners() {
+  let todoCheck = document.querySelectorAll(".todo-item .check-mark");
+  todoCheck.forEach((checkMark) => {
+    checkMark.addEventListener("click", function () {
+      markCompleted(checkMark.dataset.id)
+    })
+  })
+}
+
+
+function markCompleted(id) {
+  //vanuit de database
+  let item = db.collection("todo-items").doc(id);
+  item.get().then(function (doc) {
+    if (doc.exists) {
+      let status = doc.data().status;
+      if (status == "active") {
+        item.update({
+          status: "completed"
+        })
+      } else if (status == "completed") {
+        item.update({
+          status: "active"
+        })
+      }
+    }
+  })
+}
+
+getItems();
+
+
